5bdc0ac7b876c996b4e5c3d2fda4d16875d6eefa
Hibariya <hibariya@gmail.com>
2024-12-06 14:27:42 +0900

    Rack::Static

--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require_relative './app.rb'
 
+use Rack::Static, :urls => ["/css", "/images"], :root => "public"
 run Ishocon1::WebApp
--- puma.rb
+++ puma.rb
@@ -2,5 +2,7 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['THREAD_COUNT'] || 5)
 threads threads_count, threads_count
 
+rackup 'config.ru'
+
 port        ENV['PORT']     || 8080
 environment ENV['RACK_ENV'] || 'production'
